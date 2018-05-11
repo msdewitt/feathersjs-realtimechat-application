494cf0dfdd5baee85c16234da30d6ca2e13ff189
Matheu DeWitt <msdewitt@bsu.edu>
2018-05-11 15:43:17 -0500

    Fix wierd error.

--- src/hooks/process-message.js
+++ src/hooks/process-message.js
@@ -1,20 +1,32 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-// eslint-disable-next-line no-unused-vars
-module.exports = function (options = {}) {
+module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
-    const {data} = context;
-    if(!data.text){
+    const { data } = context;
+
+    // Throw an error if we didn't get a text
+    if(!data.text) {
       throw new Error('A message must have a text');
     }
+
+    // The authenticated user
     const user = context.params.user;
+    // The actual message text
+    const text = context.data.text
+      // Messages can't be longer than 400 characters
+      .substring(0, 400);
 
-    const text = context.data.text.substring(0,400);
-
+    // Override the original data (so that people can't submit additional stuff)
     context.data = {
       text,
+      // Set the user id
       userId: user._id,
+      // Add the current date
       createdAt: new Date().getTime()
     };
+
+    // Best practise, hooks should always return the context
     return context;
   };
 };
